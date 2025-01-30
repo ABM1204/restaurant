7bfd089a5aea804d0fdaf6af327e13257017a227
ABM1204 <melisovbz@gmail.com>
2025-01-31 01:31:37 +0600

    Добавил отображение списка блюд в странице с выручкой

--- source/orders/forms.py
+++ source/orders/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Order, OrderItem
 
+
 class OrderItemForm(forms.Form):
     name = forms.CharField(max_length=100)
     price = forms.DecimalField(max_digits=10, decimal_places=2)
@@ -23,5 +24,24 @@ class OrderForm(forms.ModelForm):
 
         if not items or not prices or not quantities:
             raise forms.ValidationError("Заполните все поля.")
+
+        try:
+            items_list = items.split(',')
+            prices_list = [float(price) for price in prices.split(',')]
+            quantities_list = [int(quantity) for quantity in quantities.split(',')]
+        except ValueError:
+            raise forms.ValidationError(
+                "Неверный формат данных. Убедитесь, что значения разделены запятыми и являются правильными числами.")
+
+        if len(items_list) != len(prices_list) or len(prices_list) != len(quantities_list):
+            raise forms.ValidationError("Количество позиций, цен и количеств не совпадает.")
+
+        for price in prices_list:
+            if price <= 0:
+                raise forms.ValidationError("Цена должна быть положительным числом.")
+
+        for quantity in quantities_list:
+            if quantity <= 0:
+                raise forms.ValidationError("Количество должно быть положительным числом.")
 
         return cleaned_data
--- source/orders/views.py
+++ source/orders/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import View, DeleteView, UpdateView, TemplateView
+
+from .forms import OrderForm
 from .models import Order, OrderItem
 
 import sys
@@ -10,41 +12,46 @@ sys.stdout.reconfigure(encoding='utf-8')
 
 class OrderCreateView(View):
     def get(self, request):
-        return render(request, 'order_create.html')
+        form = OrderForm()
+        return render(request, 'order_create.html', {'form': form})
 
     def post(self, request):
-        table_number = request.POST.get('table_number')
-        items = request.POST.getlist('items[]')
-        prices = request.POST.getlist('prices[]')
-        quantities = request.POST.getlist('quantities[]')
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            table_number = form.cleaned_data['table_number']
+            items = form.cleaned_data['items'].split(',')
+            prices = form.cleaned_data['prices'].split(',')
+            quantities = form.cleaned_data['quantities'].split(',')
 
-        if not table_number or not items or not prices or not quantities:
-            return render(request, 'order_create.html', {'error': 'Заполните все поля.'})
+            try:
+                order = Order.objects.create(table_number=table_number)
 
-        try:
-            order = Order.objects.create(table_number=table_number)
+                for item, price, quantity in zip(items, prices, quantities):
+                    try:
+                        price = float(price)
+                        quantity = int(quantity)
 
-            for item, price, quantity in zip(items, prices, quantities):
-                try:
-                    price = float(price)
-                    quantity = int(quantity)
+                        if price <= 0 or quantity <= 0:
+                            raise ValueError("Цена и количество должны быть положительными")
 
-                    if price < 0 or quantity < 0:
-                        raise ValueError("Цена и количество должны быть неотрицательными")
+                        OrderItem.objects.create(order=order, name=item, price=price, quantity=quantity)
 
-                    OrderItem.objects.create(order=order, name=item, price=price, quantity=quantity)
-                except ValueError as e:
-                    order.delete()
-                    return render(request, 'order_create.html', {'error': str(e)})
-                except Exception as e:
-                    order.delete()
-                    return render(request, 'order_create.html', {'error': f'Ошибка создания товара: {e}'})
-            order.update_total_price()
+                    except ValueError as e:
+                        order.delete()
+                        return render(request, 'order_create.html', {'form': form, 'error': str(e)})
 
-            return redirect('order_list')
+                    except Exception as e:
+                        order.delete()
+                        return render(request, 'order_create.html', {'form': form, 'error': f'Ошибка создания товара: {e}'})
 
-        except Exception as e:
-            return render(request, 'order_create.html', {'error': f'Ошибка создания заказа: {e}'})
+                order.update_total_price()
+
+                return redirect('order_list')
+
+            except Exception as e:
+                return render(request, 'order_create.html', {'form': form, 'error': f'Ошибка создания заказа: {e}'})
+        else:
+            return render(request, 'order_create.html', {'form': form, 'error': 'Заполните все поля корректно.'})
 
 
 def order_list(request):
@@ -84,4 +91,5 @@ class RevenueView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['revenue'] = Order.calculate_revenue()
+        context['orders'] = Order.objects.filter(status='оплачено')
         return context
